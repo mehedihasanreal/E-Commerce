56487c2f30e5f91c1d9249a2c802daa50ab8fc01
mehedihasanreal <mehedihasan6002@gmail.com>
2023-06-05 15:19:04 +0600

    blog and about page

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const bar = document.getElementById('bar');
 const nav = document.getElementById('navbar');
 const close = document.getElementById('close');
 
-
 if (bar){
    bar.addEventListener('click', () => {
     nav.classList.add('active');
@@ -13,5 +12,6 @@ if (bar){
 if (close){
     close.addEventListener('click', () => {
      nav.classList.remove('active');
-    }) 
- }
+   }) 
+}
+ 
